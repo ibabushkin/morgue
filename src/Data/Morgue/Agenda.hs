218a60cd1f9f2d7b27f932fba97cdfef8cdc4821
Inokentiy Babushkin <inokentiy.babushkin@googlemail.com>
2016-11-11 00:29:01 +0100

    Added some reworked parsers from times pre-rewrite

--- src/Data/Morgue/Agenda.hs
+++ src/Data/Morgue/Agenda.hs
@@ -1,7 +1,11 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Data.Morgue.Agenda where
 
 import CMark
 
+import Control.Applicative ((<|>))
+
+import Data.Attoparsec.Text
 import Data.Maybe (mapMaybe)
 import Data.Morgue.AgendaTypes
 import Data.Text (Text)
@@ -55,3 +59,29 @@ getAgendaListElem _ = Nothing
 -- | get the text from a paragraph
 getParagraphText :: [Node] -> Text
 getParagraphText ns = mconcat (map formatMarkdown ns)
+
+-- | parse a checkbox from an agenda entry description
+checkboxP :: Parser Bool
+checkboxP = (== "[x]") <$> (string "[ ]" <|> string "[x]")
+
+-- | parse a set of tags from an agenda entry description
+tagsP :: Parser [Tag]
+tagsP = colon *> sepBy (Tag <$> takeTill (== ':')) colon <* colon
+    where colon = char ':'
+
+-- | parse a repetition interval from a timestamp
+repeatP :: Parser RepeatInterval
+repeatP = string "/+" *> (Interval <$> decimal <*> timestepP)
+    where timestepP = choice
+              [ char 'd' *> pure Day
+              , char 'w' *> pure Week
+              , char 'm' *> pure Month
+              , char 'y' *> pure Year
+              ]
+
+modeP :: Parser TimeMode
+modeP = choice
+    [ char 'D' *> pure Deadline
+    , char 'T' *> pure Time
+    , char 'S' *> pure Scheduled
+    ]
