b4060a19e3890ecf4109aa2069de8ee90e009f1b
Inokentiy Babushkin <inokentiy.babushkin@googlemail.com>
2016-12-29 19:02:28 +0100

    Added `ToJSON` instances to the types used in template rendering

--- src/Data/Morgue/Agenda/Types.hs
+++ src/Data/Morgue/Agenda/Types.hs
@@ -1,18 +1,27 @@
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE OverloadedStrings #-}
 module Data.Morgue.Agenda.Types where
 
+import Data.Aeson
 import Data.Text (Text)
 import Data.Time.LocalTime (LocalTime)
 
+import GHC.Generics
+
 -- | tags are just text values "attached" to agenda elements
 newtype Tag = Tag Text
-    deriving (Show, Eq)
+    deriving (Generic, Show, Eq)
+
+instance ToJSON Tag
 
 -- | the kind of timing selected for an agenda element
 data TimeMode
     = Time -- ^ an event
     | Deadline -- ^ a task's deadline
     | Scheduled -- ^ a scheduled task
-    deriving (Show, Eq)
+    deriving (Generic, Show, Eq)
+
+instance ToJSON TimeMode
 
 -- | a time step for reoccuring events
 data TimeStep
@@ -20,13 +29,17 @@ data TimeStep
     | Week -- ^ a week (that is, 7 days for those who're unaware)
     | Month -- ^ a month in the gregorian calendar
     | Year -- ^ a year in the gregorian calendar
-    deriving (Show, Eq)
+    deriving (Generic, Show, Eq)
+
+instance ToJSON TimeStep
 
 -- | a repetition interval for reoccuring events, expressed in terms of a time step
 data RepeatInterval = Interval
     { numSteps :: Integer -- ^ the number of time steps
     , lenSteps :: TimeStep -- ^ the length of the step
-    } deriving (Show, Eq)
+    } deriving (Generic, Show, Eq)
+
+instance ToJSON RepeatInterval
 
 -- | a timestamp as defined in a markdown file
 data Timestamp = Timestamp
@@ -34,10 +47,12 @@ data Timestamp = Timestamp
     , mode :: TimeMode -- ^ the mode of the timestamp
     , repeat :: Maybe RepeatInterval -- ^ an optional repetition specifier
     , toPrint :: Bool -- ^ whether the *time* is set explicitly
-    } deriving (Show, Eq)
+    } deriving (Generic, Show, Eq)
 
 instance Ord Timestamp where
     (<=) (Timestamp a _ _ _) (Timestamp b _ _ _) = a <= b
+
+instance ToJSON Timestamp
 
 -- | the data type used to hold an element of an agenda  
 data AgendaElement = Elem
@@ -45,20 +60,34 @@ data AgendaElement = Elem
     , toDo :: Maybe Bool -- ^ an optional todo status
     , time :: Maybe Timestamp -- ^ an optional timestamp
     , tags :: [Tag] -- ^ an optional set of tags
-    } deriving (Show, Eq)
+    } deriving (Generic, Show, Eq)
 
 instance Ord AgendaElement where
     (<=) (Elem _ _ a _) (Elem _ _ b _) = a <= b
+
+instance ToJSON AgendaElement
+
+-- | a tree of hierarchically ordered agenda elements of any type
+data AgendaTree = AgendaTree AgendaElement [AgendaTree]
+    deriving (Show, Eq)
+
+instance ToJSON AgendaTree where
+    toJSON = agendaTreeToJSON 0
+
+-- | store explicit information on the indent level in the JSON representing an
+-- `AgendaTree`
+agendaTreeToJSON :: Integer -> AgendaTree -> Value
+agendaTreeToJSON n (AgendaTree e cs) = object
+    [ "element" .= e
+    , "indent" .= n
+    , "children" .= map (agendaTreeToJSON (n + 1)) cs
+    ]
 
 -- | the kinds of agenda we can get
 data AgendaMode
     = Timed Integer Bool -- ^ a timed agenda
     | Todo -- ^ a simple tree of todo items
     | Tree -- ^ a simple tree of all items
-    deriving (Show, Eq)
-
--- | a tree of hierarchically ordered agenda elements of any type
-data AgendaTree = AgendaTree AgendaElement [AgendaTree]
     deriving (Show, Eq)
 
 -- | the result type represnting an action to take when walking an `AgendaTree`
