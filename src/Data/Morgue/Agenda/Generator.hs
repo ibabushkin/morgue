00ce6630705ef9040cfc6b58e903dcc05b8e21cd
Inokentiy Babushkin <inokentiy.babushkin@googlemail.com>
2017-01-07 22:39:19 +0100

    Added basic machinery to build result types (modulo tagging)

--- src/Data/Morgue/Agenda/Generator.hs
+++ src/Data/Morgue/Agenda/Generator.hs
@@ -10,7 +10,7 @@ import Data.Morgue.Agenda.Types
 
 import GHC.Generics
 
-newtype TimedAgendaResult = TimedAgendaResult [(Day, AgendaTree)]
+newtype TimedAgendaResult = TimedAgendaResult [(Day, Maybe AgendaTree)]
 
 instance ToJSON TimedAgendaResult where
     toJSON (TimedAgendaResult days) = object [ "days" .= map pairToJSON days ]
@@ -19,10 +19,19 @@ instance ToJSON TimedAgendaResult where
                   , "tree" .= toJSON tree
                   ]
 
-newtype TodoAgendaResult = TodoAgendaResult AgendaTree
+timedAgendaResult :: Day -> Integer -> AgendaTree -> TimedAgendaResult
+timedAgendaResult day n t =
+    TimedAgendaResult $ map ((,) <$> id <*> computeTree t) (consecutiveDays day n)
+    where computeTree tree d = filterAgendaTree (agendaTreeFilterTimed False d) tree
+
+newtype TodoAgendaResult = TodoAgendaResult (Maybe AgendaTree)
 
 instance ToJSON TodoAgendaResult where
     toJSON (TodoAgendaResult tree) = object [ "tree" .= toJSON tree ]
+
+todoAgendaResult :: Bool -> AgendaTree -> TodoAgendaResult
+todoAgendaResult showDone =
+    TodoAgendaResult . filterAgendaTree (agendaTreeFilterTodo showDone)
 
 data BothAgendaResult = BothAgendaResult
     { timed :: TimedAgendaResult
@@ -31,10 +40,19 @@ data BothAgendaResult = BothAgendaResult
 
 instance ToJSON BothAgendaResult
 
-newtype TreeAgendaResult = TreeAgendaResult AgendaTree
+bothAgendaResult :: Day -> Integer -> Bool -> AgendaTree -> BothAgendaResult
+bothAgendaResult day n showDone =
+    BothAgendaResult <$> timedAgendaResult day n <*> todoAgendaResult showDone
+
+newtype TreeAgendaResult = TreeAgendaResult (Maybe AgendaTree)
 
 instance ToJSON TreeAgendaResult where
     toJSON (TreeAgendaResult tree) = object [ "tree" .= toJSON tree ]
+
+treeAgendaResult :: [Tag] -> Bool -> AgendaTree -> TreeAgendaResult
+treeAgendaResult tags invert = TreeAgendaResult . filterAgendaTree (getFilter invert tags)
+    where getFilter True = agendaTreeFilterNotTagged
+          getFilter False = agendaTreeFilterTagged
 
 -- | filter an agenda tree by dropping nodes not matched by the passed function
 filterAgendaTree :: (AgendaElement -> AgendaTreeFilter) -> AgendaTree -> Maybe AgendaTree
@@ -49,19 +67,19 @@ filterAgendaTree func tree@(AgendaTree e subTrees) =
             [] -> Nothing
             children -> Just (AgendaTree e children)
 
--- | a filter to be used to filter for subtrees tagged with certain tags
+-- | a filter for subtrees tagged with certain tags
 agendaTreeFilterTagged :: [Tag] -> AgendaElement -> AgendaTreeFilter
 agendaTreeFilterTagged allowed (Elem _ _ _ ts)
     | not. null $ allowed `intersect` ts = KeepTree
     | otherwise = DropTreeAndWalk
 
--- | a filter to be used to filter for subtrees not tagged with certain tags
+-- | a filter for subtrees not tagged with certain tags
 agendaTreeFilterNotTagged :: [Tag] -> AgendaElement -> AgendaTreeFilter
 agendaTreeFilterNotTagged forbidden (Elem _ _ _ ts)
     | not . null $ forbidden `intersect` ts = DropTree
     | otherwise = KeepTreeAndWalk
 
--- | a filter to be used to filter for subtrees denoting todo elements
+-- | a filter for subtrees denoting todo elements
 agendaTreeFilterTodo :: Bool -> AgendaElement -> AgendaTreeFilter
 agendaTreeFilterTodo _ (Elem _ Nothing _ _) = DropTreeAndWalk
 agendaTreeFilterTodo showDone (Elem _ (Just tD) _ _)
